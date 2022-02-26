d29d248c51b17e85d57534332f709a08dde8a1d4
Alon <alon@firestein.net>
2022-02-26 12:07:18 +0200

    add text to image when alarm is playing

--- camera.py
+++ camera.py
@@ -42,13 +42,16 @@ def detect_eyes(cap):
                 frame_tmp = img[face[0][1]:face[0][1] + face[0][3], face[0][0]:face[0][0] + face[0][2]:1, :]
                 frame = frame[face[0][1]:face[0][1] + face[0][3], face[0][0]:face[0][0] + face[0][2]:1]
                 eyes = open_closed.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
                 if len(eyes) == 0:
                     closed_eyes_counter += 1
                     print('ALERT: Closed eyes detected')  # Print method is only for testing
+
                 else:
                     opened_eyes_counter += 1
                     print('Open eyes detected :)')  # Print method is only for testing
 
+                print(f"Closed: {closed_eyes_counter}, Open: {opened_eyes_counter}")
                 # If our closed eyes counter meets the threshold: We play the alarm to wake up the driver!
                 if closed_eyes_counter == closed_eyes_threshold:
                     ALARM_ON = True
@@ -65,6 +68,7 @@ def detect_eyes(cap):
                 if ALARM_ON:
                     # os.system("say beep")  # On mac: Says beep when eyes closed (funny, but delete for deployment)
                     playsound.playsound("alarms/alarm_0.5seconds.wav")
+                    put_alert_text(img)
 
                 frame_tmp = cv2.resize(frame_tmp, (400, 400), interpolation=cv2.INTER_LINEAR)
                 cv2.imshow('Drowsiness Detector', frame_tmp)
@@ -76,3 +80,18 @@ def detect_eyes(cap):
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+# Put text on image when an alarm is playing to wake up the driver
+def put_alert_text(img):
+    # Text details
+    text = 'WAKE UP!'
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    fontColor = (0, 0, 255)
+    lineType = 2
+    text_width, text_height = cv2.getTextSize(text, font, fontScale, lineType)[0]
+    CenterCoordinates = (int(img.shape[1] / 2) - int(text_width / 2), int(img.shape[0] / 2) - int(text_height / 2))
+
+    return cv2.putText(img, text, CenterCoordinates, font, fontScale, fontColor, lineType)
+
