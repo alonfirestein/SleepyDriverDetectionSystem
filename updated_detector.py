1791748dd9eacfcfda537179b0f40a985e8a977f
Alon <alon@firestein.net>
2022-06-13 16:04:09 +0300

    Updating and re-organizing code to look better and into job specific functions

--- updated_detector.py
+++ updated_detector.py
@@ -37,6 +37,11 @@ left_eye_counter = {"left_eye_open": 0, "left_eye_closed": 0}
 
 
 def start_camera(URL):
+    """
+    Start the camera and return the capture object
+    :param URL: the URL of the camera to be used (0 for laptop webcam)
+    :return: the capture object
+    """
     cap = cv2.VideoCapture(URL)
     # Check if the camera is opened correctly
     if not cap.isOpened():
@@ -45,7 +50,43 @@ def start_camera(URL):
     return cap
 
 
-def update_database(file_path, columns, data):
+def update_database(file_path, started, ended, timer):
+    """
+    Update the database with the details of the latest drowsiness detection results
+    :param file_path: the path of the file to be updated
+    :param started: the time when the program started
+    :param ended: the time when the program ended
+    :param timer: a timer object
+    :return:
+    """
+    global right_eye_counter, left_eye_counter, alarm_activated_counter
+    # Total program duration in seconds
+    total_duration = float("{0:.4f}".format(time.time() - timer))
+    # Update database from last run
+    columns = ["started",
+               "ended",
+               "total_duration",
+               "right_eye_open",
+               "right_eye_closed",
+               "left_eye_open",
+               "left_eye_closed",
+               "alarm_activated_counter"]
+
+    data = [started,
+            ended,
+            total_duration,
+            right_eye_counter["right_eye_open"],
+            right_eye_counter["right_eye_closed"],
+            left_eye_counter["left_eye_open"],
+            left_eye_counter["left_eye_closed"],
+            alarm_activated_counter
+            ]
+
+    # Printing summary of the run to the console
+    print("Drowsiness Summary:")
+    for col, val in zip(columns, data):
+        print(f"{col}: {val}")
+
     if not os.path.isfile(file_path):
         df = pd.DataFrame([data], columns=columns)
         df.to_csv(file_path)
@@ -56,8 +97,76 @@ def update_database(file_path, columns, data):
             writer_object.writerow(data)
 
 
+def eye_prediction(frame, eye, prediction_list, eye_side):
+    """
+    Predict the eye state of the face (open or closed) using my trained CNN model
+    :param frame: the frame to be processed
+    :param eye: the eye classifier used to predict the eye state (using haar cascade)
+    :param prediction_list: the list to store the predictions
+    :param eye_side: which eye is being predicted (left or right)
+    :return:
+    """
+    global left_eye_counter, right_eye_counter, count
+    prediction = prediction_list
+    for (x, y, w, h) in eye:
+        eye = frame[y:y + h, x:x + w]
+        count += 1
+        eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
+        eye = cv2.resize(eye, (24, 24))
+        eye = eye / 255
+        eye = eye.reshape(24, 24, -1)
+        eye = np.expand_dims(eye, axis=0)
+        predict_x = model.predict(eye)
+        prediction = np.argmax(predict_x, axis=1)
+        if eye_side == "right":
+            if prediction.all() == 1:
+                right_eye_label = 'Open'
+                right_eye_counter["right_eye_open"] += 1
+
+            if prediction.all() == 0:
+                right_eye_label = 'Closed'
+                right_eye_counter["right_eye_closed"] += 1
+        if eye_side == "left":
+            if prediction.all() == 1:
+                left_eye_label = 'Open'
+                left_eye_counter["left_eye_open"] += 1
+
+            if prediction.all() == 0:
+                left_eye_label = 'Closed'
+                left_eye_counter["left_eye_closed"] += 1
+    return prediction
+
+
+def update_drowsiness_score(frame, right_eye_prediction, left_eye_prediction, height):
+    """
+    Update the drowsiness score based on the eye state (open or closed) and print it on the live video of the driver
+    :param frame: the frame to be processed
+    :param right_eye_prediction: the prediction of the right eye
+    :param left_eye_prediction: the prediction of the left eye
+    :param height: the height of the frame in order to print the drowsiness score on a good position
+    :return:
+    """
+    global drowsiness_score
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    cv2.putText(frame, 'Drowsiness Score:' + str(drowsiness_score), (175, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    if right_eye_prediction[0] == 0 and left_eye_prediction[0] == 0:
+        drowsiness_score += 1
+        cv2.putText(frame, "Eyes: Closed", (10, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    else:
+        drowsiness_score -= 1
+        cv2.putText(frame, "Eyes: Open", (10, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    if drowsiness_score < 0:
+        drowsiness_score = 0
+
+
 def detection(cap):
-    global camera_open, ALARM_ON, drowsiness_score, count, right_eye_counter,\
+    """
+    The main function of the program. It is responsible for detecting the drowsiness of the driver.
+    :param cap: the capture object used to capture the video of the driver
+    :return:
+    """
+    global camera_open, ALARM_ON, drowsiness_score, right_eye_counter,\
            left_eye_counter, alarm_activated_counter, captured_photo
     started, timer = datetime.now().strftime('%Y-%m-%d %H:%M:%S'), time.time()
 
@@ -100,57 +209,12 @@ def detection(cap):
         #     center_coordinates = (int(x+w//2), int(y+ey//2))
         #     cv2.circle(frame, center_coordinates, radius, (0, 0, 255), 2)
 
-        for (x, y, w, h) in right_eye:
-            r_eye = frame[y:y + h, x:x + w]
-            count += 1
-            r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
-            r_eye = cv2.resize(r_eye, (24, 24))
-            r_eye = r_eye / 255
-            r_eye = r_eye.reshape(24, 24, -1)
-            r_eye = np.expand_dims(r_eye, axis=0)
-            predict_x = model.predict(r_eye)
-            right_eye_prediction = np.argmax(predict_x, axis=1)
-            if right_eye_prediction.all() == 1:
-                right_eye_label = 'Open'
-                right_eye_counter["right_eye_open"] += 1
+        # Predict the eye state of the face (open or closed)
+        left_eye_prediction = eye_prediction(frame, left_eye, left_eye_prediction, eye_side="left")
+        right_eye_prediction = eye_prediction(frame, right_eye, right_eye_prediction, eye_side="right")
 
-            if right_eye_prediction.all() == 0:
-                right_eye_label = 'Closed'
-                right_eye_counter["right_eye_closed"] += 1
-            break
-
-        for (x, y, w, h) in left_eye:
-            l_eye = frame[y:y + h, x:x + w]
-            count += 1
-            l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2GRAY)
-            l_eye = cv2.resize(l_eye, (24, 24))
-            l_eye = l_eye / 255
-            l_eye = l_eye.reshape(24, 24, -1)
-            l_eye = np.expand_dims(l_eye, axis=0)
-            predict_x = model.predict(l_eye)
-            left_eye_prediction = np.argmax(predict_x, axis=1)
-
-            if left_eye_prediction.all() == 1:
-                left_eye_label = 'Open'
-                left_eye_counter["left_eye_open"] += 1
-
-            if left_eye_prediction.all() == 0:
-                left_eye_label = 'Closed'
-                left_eye_counter["left_eye_closed"] += 1
-            break
-
-        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-        if right_eye_prediction[0] == 0 and left_eye_prediction[0] == 0:
-            drowsiness_score += 1
-            cv2.putText(frame, "Eyes: Closed", (10, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-        else:
-            drowsiness_score -= 1
-            cv2.putText(frame, "Eyes: Open", (10, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-
-        if drowsiness_score < 0:
-            drowsiness_score = 0
-
-        cv2.putText(frame, 'Drowsiness Score:' + str(drowsiness_score), (175, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+        # Updating the drowsiness score and also printing it on the live video of the driver
+        update_drowsiness_score(frame, right_eye_prediction, left_eye_prediction, height)
 
         # If we detect that the driver is sleepy, we play the alarm to wake them up
         if drowsiness_score >= closed_eyes_threshold:
@@ -160,7 +224,8 @@ def detection(cap):
                 put_alert_text(frame)
                 # Capturing a photo of the sleepy driver as proof of their drowsiness
                 if not captured_photo:
-                    cv2.imwrite(os.path.join(path, f'images/sleeping_driver-{started}.jpg'), frame)
+                    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    cv2.imwrite(os.path.join(path, f"images/sleeping_driver-{current_time}.jpg"), frame)
                     captured_photo = True
 
             except Exception as e:
@@ -173,33 +238,10 @@ def detection(cap):
 
     cap.release()
     cv2.destroyAllWindows()
+
     ended = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Total program duration in seconds
-    total_duration = float("{0:.4f}".format(time.time() - timer))
-    # Update database from last run
-    columns = ["started",
-               "ended",
-               "total_duration",
-               "right_eye_open",
-               "right_eye_closed",
-               "left_eye_open",
-               "left_eye_closed",
-               "alarm_activated_counter"]
-
-    data = [started,
-            ended,
-            total_duration,
-            right_eye_counter["right_eye_open"],
-            right_eye_counter["right_eye_closed"],
-            left_eye_counter["left_eye_open"],
-            left_eye_counter["left_eye_closed"],
-            alarm_activated_counter
-            ]
-
-    print("Drowsiness Summary:")
-    for col, val in zip(columns, data):
-        print(f"{col}: {val}")
-    update_database("drowsiness_detection_data.csv", columns, data)
+    # Updating database with all the information about the last session
+    update_database("drowsiness_detection_data.csv", started, ended, timer)
     camera_open = False
 
 
